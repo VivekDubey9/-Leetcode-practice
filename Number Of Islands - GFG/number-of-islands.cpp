fa58a088c5810eb05f59fcced42cf627b39440de
Vivek Dubey <72728328+VivekDubey9@users.noreply.github.com>
2023-03-15 17:14:50 +0530

    Added solution - LeetHub

---
+++ Number Of Islands - GFG/number-of-islands.cpp
@@ -0,0 +1,114 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+class Solution {
+  public:
+    vector<int> numOfIslands(int n, int m, vector<vector<int>> &operators) {
+        // code here
+        int cnt =0;
+        DisjointSet ds(n*m);
+       
+        int vis[n][m];
+        memset(vis,0,sizeof vis);
+        vector<int> ans;
+        for(int i=0;i<operators.size();i++){
+            int row = operators[i][0];
+            int col = operators[i][1];
+            if(vis[row][col]==1){
+                ans.push_back(cnt);
+                continue;
+            }
+            int node = row*m + col;
+            vis[row][col] =1;
+            cnt++;
+             int dr[]={-1,0,1,0};
+             int dc[] ={0,1,0,-1};
+            for(int ind=0;ind<4;ind++){
+                int nrow = row+dr[ind];
+                int ncol = col+dc[ind];
+                int newNode = nrow*m +ncol;
+                if(nrow<n && nrow>=0 && ncol<m && ncol>=0){
+                    
+                    if(vis[nrow][ncol]==1 && ds.findUPar(node)!=ds.findUPar(newNode)){
+                        cnt--;
+                        ds.unionBySize(node,newNode);
+                    }
+                
+                }
+                    
+                
+            }
+            ans.push_back(cnt);
+        }
+        return ans;
+        
+        
+        
+    } 
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,m,k; cin>>n>>m>>k;
+        vector<vector<int>> a;
+        
+        for(int i=0; i<k; i++){
+            vector<int> temp;
+            for(int j=0; j<2; j++){
+                int x; cin>>x;
+                temp.push_back(x);
+            }
+            a.push_back(temp);
+        }
+    
+        Solution obj;
+        vector<int> res = obj.numOfIslands(n,m,a);
+        
+        for(auto x : res)cout<<x<<" ";
+        cout<<"\n";
+    }
+}
+
+// } Driver Code Ends
