d7032bae9803fe41cc699affdaa608c40198a3f5
Vivek Dubey <72728328+VivekDubey9@users.noreply.github.com>
2023-04-12 16:00:50 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
@@ -19,8 +19,18 @@ public:
 	}
 	int findMaxSum(int *arr, int n) {
 	    // code here
-	    vector<int> dp(n+1,-1);
-	    return solve(arr,0,n,dp);
+	    vector<int> dp(n,0);
+	   // return solve(arr,0,n,dp);
+	   dp[0]=arr[0];
+	   for(int i=1;i<n;i++){
+	       int take = arr[i];
+	       if(i-2>=0){
+	           take+=dp[i-2];
+	       }
+	       int not_take = dp[i-1];
+	       dp[i] = max(take,not_take);
+	   }
+	   return dp[n-1];
 	    
 	}
 };
